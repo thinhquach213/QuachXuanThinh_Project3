006b5dc8f922e40f99c6192d1af10f41190609ae
unknown <quachxuanthinh20092005@gmail.com>
2025-10-21 21:05:32 +0700

    update

---
+++ lesson1-spring-boot/src/main/java/com/devmaster/lesson1_spring_boot/Lesson1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devmaster.lesson1_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson1SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson1SpringBootApplication.class, args);
+	}
+
+}
